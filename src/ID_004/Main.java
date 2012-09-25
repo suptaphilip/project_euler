6c84d4a6a9d31d2695682877839de71226024c4e
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2012-09-25 08:30:14 -0400

    Revisions made to main to support decrement of counter.

--- src/ID_004/Main.java
+++ src/ID_004/Main.java
@@ -1,114 +1,61 @@
 package ID_004;
 
-
 /**
  * Find the largest palindrome made from the product of two 3-digit numbers.
  */
 
-
 public class Main {
 
-	
+	public final static int max = 999;
 
-public final static int max = 999;
+	public static void main(String[] args) {
+		int n = max;
 
-
-	
-public static void main(String[] args) {
-	
-	int n = max;
-
-		
-	while (n > 0) 
-	{
-		
-		int temp = getProduct(n);
-	
-		if (temp != 0) 
-		{
-	
-		    	int m = temp/n;
-	
-			System.out.println(n + " X " + m);
-
-			break;
+		while (n > 0) {
+			int temp = getProduct(n);
+			if (temp != 0) {
+				System.out.println(temp);
+				break;
+			} else {
+				n--;
+			}
 		}
-
-		else
-
-		{
-
-		   n--;
-			
-		}
-
 	}
 
-}
+	public static int getProduct(int n) {
 
-	
+		int result = 0; // is acting like a boolean
 
-
-public static int getProduct(int n) {
-
-	int result = 0; // is acting like a boolean
-
-		for (int i = max; i >= n; i--) 
-		{
-
+		for (int i = max; i >= n; i--) {
 			int product = n * i;
 
-			if (isPalidrome(product)) 
-			{
-
+			if (isPalidrome(product)) {
 				return product;
-			
 			}
-
 		}
-
-	return result;
-
-}
-
-
-
-public static boolean isPalidrome(int p) {
-	
-	String stringNumber = String.valueOf(p);
-	
-	int length = stringNumber.length();
-		
-	char[] array = new char[length];
-	
-	char[] yarra = new char[length];
-
-	
-
-	for (int i = 0; i < length; i++) {
-		
-		array[i] = stringNumber.charAt(i);
-		
-		yarra[length - (i + 1)] = array[i];
-		
+		return result;
 	}
 
-		
+	public static boolean isPalidrome(int p) {
 
-	for (int i = 0; i < length; i++) {
-	
-		if (array[i] != yarra[i]) 
-		{
+		String stringNumber = String.valueOf(p);
+		int length = stringNumber.length();
 
-			return false;
-		
+		char[] array = new char[length];
+		char[] yarra = new char[length];
+
+		for (int i = 0; i < length; i++) {
+
+			array[i] = stringNumber.charAt(i);
+			yarra[length - (i + 1)] = array[i];
 		}
 
+		for (int i = 0; i < length; i++) {
+
+			if (array[i] != yarra[i]) {
+				return false;
+			}
+		}
+		return true;
 	}
-
-	return true;
-
-}
-
-
 }
