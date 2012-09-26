b9041aeb13c841ee95b82b0ac043d519284342dd
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2012-09-26 09:39:45 -0400

    Last commit broke JUnit test. TEST FIRST DESIGN.

--- src/ID_005/Main.java
+++ src/ID_005/Main.java
@@ -4,20 +4,20 @@ package ID_005;
  * What is the smallest number divisible by each of the numbers 1 to 20?
  */
 public class Main {
-	static int[] values = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
-			13, 14, 15, 16, 17, 18, 19, 20 };
 
 	public static void main(String[] args) {
+		int[] values = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+				14, 15, 16, 17, 18, 19, 20 };
 
-		System.out.println(calculateMin());
+		System.out.println(calculateMin(values));
 	}
 
-	public static int calculateMin() {
+	public static int calculateMin(int[] values) {
 		int result = 1;
 		for (int i = 0; i < values.length; i++) {
 
 			int divisor = largestPrime(values[i]);
-			values = divide(divisor);
+			values = divide(values, divisor);
 			result = result * divisor;
 		}
 		return result;
@@ -33,7 +33,7 @@ public class Main {
 		return MaxPrime;
 	}
 
-	public static int[] divide(int d) {
+	public static int[] divide(int[] values, int d) {
 		for (int i = 0; i < values.length; i++) {
 			if (values[i] % d == 0) {
 				values[i] = values[i] / d;
--- test/ID_005/MainTest.java
+++ test/ID_005/MainTest.java
@@ -11,4 +11,9 @@ public class MainTest {
 		int[] temp = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 		assertEquals("Result", 2520, Main.calculateMin(temp));
 	}
+
+	@Test
+	public void testPrime() {
+		assertEquals("Result", 41, Main.largestPrime(82));
+	}
 }
