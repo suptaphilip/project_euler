6e25b16b313f7462a67a508f209e8b06bb936c22
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2012-09-26 09:47:12 -0400

    Helper method, getDivisor, is not needed.

--- src/ID_003/Main.java
+++ src/ID_003/Main.java
@@ -5,15 +5,10 @@ import java.util.Scanner;
 /**
  * Find the largest prime factor a composite number.
  * 
- * The integer n is composite if and only if there exists an integer a such that
- * a|n and 1<a<n
  */
 
 public class Main {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
@@ -29,23 +24,10 @@ public class Main {
 				if (compositeN == 1) {
 					System.out.println("No prime factors.");
 				} else {
-					int divisor = largestDivisor(compositeN);
-
-					System.out.println(getPrimeFact(divisor));
+					System.out.println(getPrimeFact(compositeN));
 				}
 			}
 		}
-	}
-
-	public static int largestDivisor(int compositeN) {
-		int divisor = 1;
-
-		for (int i = 2; i < compositeN; i++) {
-			if (compositeN % i == 0) {
-				divisor = i;
-			}
-		}
-		return divisor;
 	}
 
 	public static int getPrimeFact(int d) {
